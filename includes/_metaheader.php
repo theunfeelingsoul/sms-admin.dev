3f449f06c884f10c02f3c518adda3bfebd035c28
Angel Beats <victorstinefuture@gmail.com>
2016-01-19 14:40:18 +0900

    favicon

--- includes/_metaheader.php
+++ includes/_metaheader.php
@@ -2,3 +2,5 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+<link rel="icon" href="dist/img/faviconv.ico" type="image/gif" sizes="16x16">

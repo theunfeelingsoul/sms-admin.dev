bdbb0d990d7c49f3607ca8759bd6460b5a49507c
Angel Beats <victorstinefuture@gmail.com>
2016-01-19 13:51:16 +0900

    edited images
    Julie gischuru

--- includes/_header.php
+++ includes/_header.php
@@ -1,6 +1,6 @@
 <header class="main-header">
         <!-- Logo -->
-        <a href="../../index2.html" class="logo">
+        <a href="" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>A</b>LT</span>
           <!-- logo for regular state and mobile devices -->
@@ -31,7 +31,7 @@
                       <li><!-- start message -->
                         <a href="#">
                           <div class="pull-left">
-                            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                            <img src="dist/img/julie.jpg" class="img-circle" alt="User Image">
                           </div>
                           <h4>
                             Support Team
@@ -208,8 +208,8 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                  <span class="hidden-xs">Alexander Pierce</span>
+                  <img src="dist/img/julie.jpg" class="user-image" alt="User Image">
+                  <span class="hidden-xs">Julie Gichuru</span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
